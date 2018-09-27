982961dab63f504357133e53230e1f45c494e1f0
angela <angela.martoccia-boyadjian@epitech.eu>
2018-09-27 22:41:39 +0200

    [COMPILE]

---
+++ dist/Minify.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: Angela
+ * @Date: 2018-09-27 22:28:39
+ * @Last Modified by: Angela
+ * @Last Modified time: 2018-09-27 22:35:28
+ */
+class Minify {
+    constructor(file) {
+        this.fileName = file;
+    }
+    getResult() {
+        return this.result;
+    }
+    printResult() {
+        console.log(this.result);
+    }
+}
+//# sourceMappingURL=Minify.js.map
