9b35912fb29562394175e114508dbcc16071294a
angela-boyadjian <boyadjian.angela@gmail.com>
2018-10-02 14:20:57 +0200

    [FEATURE] Now also minifies variable names

--- MinifyJS/srcs/Minify.js
+++ MinifyJS/srcs/Minify.js
@@ -11,8 +11,12 @@ class Minify {
 	
 	constructor(file) {
 		this.filePath = file;
+		this.fileContent = "";
 		this.error = false;
 		this.array = [];
+		this.mapVar = new Map();
+		this.key = 'a';
+		this.mapVar.set(this.key, "");
 	}
 
 	printResult() {
@@ -51,17 +55,70 @@ class Minify {
 		return 1;
 	}
 
+	isAlphaNum(ch) {
+		return ch.match(/^[a-z0-9]+$/i) !== null;
+	}
+	
 	initArray() {
 		var save = "";
 
 		for (var i = 0; this.fileContent[i]; ++i) {
-			while (this.fileContent[i] && this.fileContent[i] !== ' '
-				&& this.fileContent[i] !== '\n' && this.fileContent[i] !== '\t')
-				save += this.fileContent[i++];
-			if (save !== "")
+			if (!this.isAlphaNum(this.fileContent[i])) {
+				if (this.fileContent[i] === ' ')
+				continue;
+				if (this.fileContent[i] === '\n')
+					continue;
+				save += this.fileContent[i];
+				if (save !== '\t')
+					this.array.push(save);
+				save = ""
+			} else {
+				while (this.isAlphaNum(this.fileContent[i]))
+					save += this.fileContent[i++];
 				this.array.push(save);
-			save = "";
+				save = ""
+			}
 		}
+	}
+
+	getKey(str) {
+		for (var [key, value] of this.mapVar) {
+			if (str === value)
+				return key;
+		}
+		return -1;
+	}
+
+	incrementKey() {
+		switch (this.key) {
+			case 'a':
+				this.key = 'b';
+				break;
+			case 'b':
+				this.key = 'c';
+				break;
+			case 'c':
+				this.key = 'd';
+				break;
+			case 'd':
+				this.key = 'e';
+				break;
+			case 'e':
+				this.key = 'f';
+				break;
+			case 'f':
+				this.key = 'g';
+				break;
+			default:
+				this.key = 'a';
+				break;
+		}
+	}
+
+	handleVariables(i) {
+		this.mapVar.set(this.key, this.array[i]);
+		this.result += this.key;
+		this.incrementKey();
 	}
 
 	minify() {
@@ -70,14 +127,28 @@ class Minify {
 		if (!this.error) {
 			this.handleHeader();
 			this.initArray();
-			this.result = this.array[0];
-			this.result += " ";
-			for (var i = 1; i < this.array.length; ++i) {
+			this.result = "";
+			var i = 1;
+			if (this.array[0] === "var") {
+				this.handleVariables(1);
+				++i;
+			} else {
+				this.result = this.array[0];
+			}
+			for (i; i < this.array.length; ++i) {
 				if (this.array[i] === "var") {
-					this.result += " ";
+					this.handleVariables(i + 1);
+					++i;
+				} else {
+					var key = this.getKey(this.array[i]);
 
+					if (key != -1) { {
+						this.result += key;
+					}
+					} else {
+						this.result += this.array[i];
+					}
 				}
-				this.result += this.array[i];
 			}
 			return this.result;
 		}
